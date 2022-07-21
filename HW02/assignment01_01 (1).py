1672a8e45aa1098a36d0fb0e12da55c35e18fd05
Mykola Halaiko <justlite@live.com>
2022-07-21 13:30:40 +0400

    Home work #2, first commit

---
+++ HW02/assignment01_01 (1).py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Assignment 01-01
+#
+# Assign actual data to the variables below
+# and run the script to print the output.
+
+name = "Mykola"
+profession = "qa"
+facility = "Woub.nl"
+city = "Hague"
+country = "Holland"
+goal = "Automation qa engineer"
+age = 30
+
+print(
+f"""
+My name is {name}. I am a {profession} at {facility} 
+in {city}, {country}. I want to learn how to program
+in Python to become a {goal}. I am {age} years old.
+"""
+) 
